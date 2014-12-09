1dfcfaf1b6ac497e02484ceba57369aa6a9e681d
Juan Pablo Prado <jprado@sunset.com.mx>
2014-12-09 16:51:55 -0600

    Added logging

--- src/main/java/com/example/helloworld/App.java
+++ src/main/java/com/example/helloworld/App.java
@@ -5,6 +5,8 @@ import io.dropwizard.Application;
 import io.dropwizard.Configuration;
 import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Hello world!
@@ -12,6 +14,8 @@ import io.dropwizard.setup.Environment;
  */
 public class App extends Application<Configuration>
 {
+    private static final Logger LOGGER = LoggerFactory.getLogger(App.class);
+
     public static void main( String[] args ) throws Exception {
         new App().run(args);
     }
@@ -23,6 +27,8 @@ public class App extends Application<Configuration>
 
     @Override
     public void run(Configuration configuration, Environment environment) throws Exception {
+        LOGGER.info("Method App#run() called");
+        System.out.println( "Hello world, by Dropwizard!" );
         final HelloWorldResource resource = new HelloWorldResource();
         environment.jersey().register(resource);
     }
